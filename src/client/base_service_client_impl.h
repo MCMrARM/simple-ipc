16f503aacfad3dee1fa7b8d43c7a0ce93cc6cff1
MCMrARM <mcmrarm@gmail.com>
2018-05-24 21:45:06 +0200

    Revert "service_client_impl::set_reconnect_handler"
    This reverts commit dcfe8e1966befd00b59a91d47aba900cc64cb972.

--- src/client/base_service_client_impl.h
+++ src/client/base_service_client_impl.h
@@ -9,23 +9,16 @@ class base_service_client_impl : public service_client_impl {
 
 protected:
     callback_interface* cb = nullptr;
-    std::function<void ()> cb_reconnect;
 
 public:
     void set_callback_interface(callback_interface* cb) override {
         this->cb = cb;
     }
 
-    void set_reconnect_handler(std::function<void ()> f) override {
-        cb_reconnect = f;
-    }
-
     void connection_closed(connection&) override {
         close();
         if (cb)
             cb->connection_closed();
-        if (cb_reconnect)
-            cb_reconnect();
     }
 
     void handle_message(connection&, response_message const& msg) override {
